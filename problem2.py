b20e0bda73c5e0c772e7d36318dd3898d062f8a8
Angeloc1400 <115585941+Angeloc1400@users.noreply.github.com>
2022-12-08 19:06:23 -0600

    Delete problem2.py

--- problem2.py
+++
@@ -1,6 +0,0 @@
-#Angel Ocampo
-#November 1, 2022
-#prints number of th4 lists and finds the square root
-num_list = [12, 10, 32, 3, 66, 17, 42, 99, 20]
-for n in num_list:
-    print(n), print (n**2)
